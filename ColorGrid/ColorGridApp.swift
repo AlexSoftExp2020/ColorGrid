e6b8d7db4bfe490e4582c49dfa6f6d7c652fe95b
Alex <oleksii.pidhornyi@gmail.com>
2024-12-20 12:46:08 +0100

    Initial Commit

---
+++ ColorGrid/ColorGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGridApp.swift
+//  ColorGrid
+//
+//  Created by Oleksii on 20.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
